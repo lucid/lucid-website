8c20f293ae22b2f6d99fa7e259d71babadd65e4d
HFLW <me@hflw.org>
2008-08-13 15:28:28 +0000

    worked on package repositories for the website 2000 GET!!!
    git-svn-id: http://svn.psychdesktop.net/website/trunk@2000 815afad1-63b8-44ff-8d19-411ecde00fa5

---
+++ desktopsite/apps/repository/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Package(models.Model):
+    sysname = models.SlugField("System Name", unique=True)
+    name = models.CharField(maxlength=200)
+    description = models.TextField()
+    url = models.URLField("Homepage")
+    maintainer = models.ForeignKey(User)
+    type = models.CharField(maxlength=100, choices=("Application", "Theme", "Translation"))
+    def get_rating(self):
+        ratings = self.rating_set.all()
+        
+            
+    
+class Version(models.Model):
+    package = models.FireignKey(Package)
+    creation_date = models.DateField(auto_now=True, editable=False)
+    name = models.CharField(max_length=100)
+    checksum = models.CharField(max_length=100, editable=False)
+    package_url = models.URLField()
+    verified_safe = models.BooleanField()
+
+class Rating(models.Model):
+    version=models.ForeignKey(Version)
+    rating=models.IntegerField(choices=(1,2,3,4,5))
