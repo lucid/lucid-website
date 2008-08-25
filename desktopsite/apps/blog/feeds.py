74fe0237863f3d4f49aeabab37d21f60cf4ffece
HFLW <me@hflw.org>
2008-08-25 22:32:12 +0000

    Some stuff involving the feed for the blog
    git-svn-id: http://svn.psychdesktop.net/website/trunk@2024 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktopsite/apps/blog/feeds.py
+++ desktopsite/apps/blog/feeds.py
@@ -6,9 +6,15 @@ class WeblogEntryFeed(Feed):
     title = "Lucid Desktop Project Blog"
     link = "http://www.lucid-desktop.org/blog/"
     description = "Latest news about the Lucid Desktop Project."
-
+    
+    title_template = "blog/feeds/title.html"
+    description_template = "blog/feeds/description.html"
+    
     def items(self):
         return Entry.objects.filter(pub_date__lte=datetime.datetime.now())[:10]
 
     def item_pubdate(self, item):
         return item.pub_date
+    
+    def item_description(self, item):
+        return item.body
