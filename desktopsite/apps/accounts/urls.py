bd79ea41c332b2d5da508fe70c8f934a9b165b6a
HFLW <me@hflw.org>
2008-09-30 19:13:54 +0000

    Fixes #131
    git-svn-id: http://svn.psychdesktop.net/website/trunk@2105 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktopsite/apps/accounts/urls.py
+++ desktopsite/apps/accounts/urls.py
@@ -2,7 +2,13 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^accounts/profile/$', 'desktopsite.apps.accounts.views.current_user_profile', name="accounts-current-user-profile"),
+    url(r'^accounts/profile/edit/$', 'desktopsite.apps.accounts.views.edit_profile', name="accounts-edit-profile"),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', name="accounts-login"),
+    url(r'^accounts/password_change/$', 'django.contrib.auth.views.password_change'),
+    url(r'^accounts/password_change_done/$', 'django.contrib.auth.views.password_change_done'),
+    url(r'^accounts/password_reset/$', 'django.contrib.auth.views.password_reset'),
+    url(r'^accounts/password_reset_confirm/(?P<token>[\w-]+)/(?P<uidb36>[\w-]+)/$', 'django.contrib.auth.views.password_reset_confirm'),
+    url(r'^accounts/password_reset_done/$', 'django.contrib.auth.views.password_reset_done'),
     url(r'^accounts/$', 'django.contrib.auth.views.login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', name="accounts-logout"),
     url(r'^user/(?P<username>[\w-]+)/$', 'desktopsite.apps.accounts.views.profile', name="accounts-profile"),
--- desktopsite/apps/accounts/views.py
+++ desktopsite/apps/accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
 from desktopsite.apps.snapboard.models import SnapboardProfile
+from desktopsite.apps.accounts.forms import *
 
 @login_required
 def current_user_profile(request):
@@ -20,3 +21,18 @@ def profile(request, username):
         'forum': forum,
         'is_owner': request.user.username == username,
     }, context_instance=RequestContext(request))
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            request.user.message_set.create(message='Profile Changes Saved')
+            return HttpResponseRedirect("/accounts/profile/")
+    else:
+        form = ProfileForm(instance=request.user)
+    return render_to_response("accounts/editprofile.html",{
+        'form': form,
+    }, context_instance=RequestContext(request))
+
