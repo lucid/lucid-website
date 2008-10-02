7b8d44a43a27585e59c0e3669e223ee86fe27c52
HFLW <me@hflw.org>
2008-10-02 01:22:34 +0000

    Wrote an application tutorial
    git-svn-id: http://svn.psychdesktop.net/website/trunk@2112 815afad1-63b8-44ff-8d19-411ecde00fa5

--- desktopsite/dojango/media/dojo/1.2.0rc2/lucidsite/documentation/_base.js
+++ desktopsite/dojango/media/dojo/1.2.0rc2/lucidsite/documentation/_base.js
@@ -4,6 +4,9 @@ dojo.require("dojox.highlight.languages._www");
 dojo.require("dojox.highlight.languages.pygments._www");
 
 var initCode = function(){
-	dojo.query("code").forEach(dojox.highlight.init);
+	dojo.query("code").forEach(function(node) {
+	    node.innerHTML = node.innerHTML.replace(/\</gi,"&lt;").replace(/\</gi,"&gt;").replace(/\&/gi,"&amp;")
+	    dojox.highlight.init(node);
+	});
 };	
 dojo.addOnLoad(initCode); 
