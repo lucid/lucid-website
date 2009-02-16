5c5a76ccbfeb3113ac51794cd51bcbeae0b58fa3
HFLW <me@hflw.org>
2009-02-16 17:21:47 -0500

    fix to settings

--- desktopdocs/settings.py
+++ desktopdocs/settings.py
@@ -3,7 +3,7 @@ from desktopsite.settings import *
 PREPEND_WWW = False
 APPEND_SLASH = True
 INSTALLED_APPS = ['desktopdocs']
-TEMPLATE_DIRS = [os.path.join(os.path.dirname(__file__), "templates")] + TEMPLATE_DIRS
+TEMPLATE_DIRS = [os.path.join(os.path.dirname(__file__), "templates")] + list(TEMPLATE_DIRS)
 TEMPLATE_CONTEXT_PROCESSORS = ['django.core.context_processors.request']
 ROOT_URLCONF = 'desktopdocs.urls'
 CACHE_MIDDLEWARE_KEY_PREFIX = 'desktopdocs'
