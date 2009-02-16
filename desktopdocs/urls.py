192605c0bfc393a1bcf3b3344af4d9f6c74fe499
HFLW <me@hflw.org>
2009-02-16 17:46:32 -0500

    fix to urls

--- desktopdocs/urls.py
+++ desktopdocs/urls.py
@@ -1,36 +1,38 @@
 import desktopdocs.views
 from django.conf.urls.defaults import *
 
+prefix = "documentation/"
+
 urlpatterns = patterns('',
     url(
-        r'^$',
+        r'^%s$' % prefix,
         desktopdocs.views.index,
     ),
     url(
-        r'^(?P<lang>[a-z-]+)/$',
+        r'^%s(?P<lang>[a-z-]+)/$' % prefix,
         desktopdocs.views.language,
     ),
     url(
-        r'^(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/$',
+        r'^%s(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/$' % prefix,
         desktopdocs.views.document,
         {'url': ''},
         name = 'document-index',
     ),
     url(
-        r'^(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/search/$',
+        r'^%s(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/search/$' % prefix,
         desktopdocs.views.search,
         name = 'document-search',
     ),
     url(
-        r'^(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/_images/(?P<path>.*)$',
+        r'^%s(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/_images/(?P<path>.*)$' % prefix,
         desktopdocs.views.images,
     ),
     url(
-        r'^(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/_source/(?P<path>.*)$',
+        r'^%s(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/_source/(?P<path>.*)$' % prefix,
         desktopdocs.views.source,
     ),
     url(
-        r'^(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/(?P<url>[\w./-]*)/$',
+        r'^%s(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/(?P<url>[\w./-]*)/$' % prefix,
         desktopdocs.views.document,
         name = 'document-detail',
     ),
