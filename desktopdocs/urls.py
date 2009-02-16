8829b8e39c90c42df9264529325f95424cce8528
HFLW <me@hflw.org>
2009-02-16 17:32:59 -0500

    fix to urls.py

--- desktopdocs/urls.py
+++ desktopdocs/urls.py
@@ -4,34 +4,34 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     url(
         r'^$',
-        djangodocs.views.index,
+        desktopdocs.views.index,
     ),
     url(
         r'^(?P<lang>[a-z-]+)/$',
-        djangodocs.views.language,
+        desktopdocs.views.language,
     ),
     url(
         r'^(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/$',
-        djangodocs.views.document,
+        desktopdocs.views.document,
         {'url': ''},
         name = 'document-index',
     ),
     url(
         r'^(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/search/$',
-        djangodocs.views.search,
+        desktopdocs.views.search,
         name = 'document-search',
     ),
     url(
         r'^(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/_images/(?P<path>.*)$',
-        djangodocs.views.images,
+        desktopdocs.views.images,
     ),
     url(
         r'^(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/_source/(?P<path>.*)$',
-        djangodocs.views.source,
+        desktopdocs.views.source,
     ),
     url(
         r'^(?P<lang>[a-z-]+)/(?P<version>[\w.-]+)/(?P<url>[\w./-]*)/$',
-        djangodocs.views.document,
+        desktopdocs.views.document,
         name = 'document-detail',
     ),
 )
