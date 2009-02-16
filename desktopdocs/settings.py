199bdc2b87792669001013f30e7657158c053213
HFLW <me@hflw.org>
2009-02-16 17:50:45 -0500

    added dojango to installed apps

--- desktopdocs/settings.py
+++ desktopdocs/settings.py
@@ -2,7 +2,7 @@ from desktopsite.settings import *
 
 PREPEND_WWW = False
 APPEND_SLASH = True
-INSTALLED_APPS = ['desktopdocs']
+INSTALLED_APPS = ['desktopdocs', 'dojango']
 TEMPLATE_DIRS = [os.path.join(os.path.dirname(__file__), "templates")] + list(TEMPLATE_DIRS)
 TEMPLATE_CONTEXT_PROCESSORS = ['django.core.context_processors.request']
 ROOT_URLCONF = 'desktopdocs.urls'
