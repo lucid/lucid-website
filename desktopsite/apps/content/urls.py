41982bd27298cc69182541d2f46e5cef20f39d45
HFLW <me@hflw.org>
2009-02-18 20:27:36 -0500

    Added google custom search to site

--- desktopsite/apps/content/urls.py
+++ desktopsite/apps/content/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     url(r'^$', "desktopsite.apps.content.views.index", name="content-index"),
+    url(r'^search/$', "desktopsite.apps.content.views.search", name="search"),
 )
--- desktopsite/apps/content/views.py
+++ desktopsite/apps/content/views.py
@@ -5,3 +5,8 @@ from desktopsite.apps.blog.models import Entry as BlogEntry
 
 def index(request):
     return render_to_response('content/index.html', {}, context_instance=RequestContext(request))
+
+def search(request):
+    return render_to_response('content/search.html', {
+        "query": request.GET['q'],
+    }, context_instance=RequestContext(request))
