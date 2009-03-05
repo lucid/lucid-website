4981325548b6dd0c1ad3da8d15cca59c61ab387f
HFLW <me@hflw.org>
2009-03-04 21:26:06 -0500

    Pimped the video so we don't have to rely on vimeo

--- desktopsite/dojango/media/dojo/1.2.0/lucidsite/index.js
+++ desktopsite/dojango/media/dojo/1.2.0/lucidsite/index.js
@@ -1,3 +1,22 @@
 dojo.provide("lucidsite.index");
 dojo.require("dijit.layout.TabContainer");
 dojo.require("dijit.layout.ContentPane");
+dojo.require("dojox.widget.Dialog");
+dojo.require("dojox.av.FLVideo");
+dojo.require("dojox.av.widget.Player");
+dojo.require("dojox.av.widget.PlayButton");
+dojo.require("dojox.av.widget.VolumeButton");
+dojo.require("dojox.av.widget.ProgressSlider");
+dojo.require("dojox.av.widget.Status");
+
+dojo.addOnLoad(function(){
+    dojo.query(".PlayerScreen", "intro-video-player").style({width: "896px", height: "504px"});
+    dojo.connect(dijit.byId("intro-video-dialog"), "show", function(){
+        setTimeout(dojo.hitch(dijit.byId("intro-video-player").children[0], "onClick"), 2000);
+    });
+    dojo.connect(dijit.byId("intro-video-dialog"), "hide", function(){
+        var v = dijit.byId("intro-video-player");
+        if(v.media.isPlaying) v.children[0].onClick();
+    });
+});
+
