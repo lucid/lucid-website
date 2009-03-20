d88e4e0d0a7038779bf238a53532c730d479d9f3
HFLW <me@hflw.org>
2009-03-20 19:11:38 -0400

    Improvements to the way the video is presented

--- desktopsite/dojango/media/dojo/1.3.0/lucidsite/index.js
+++ desktopsite/dojango/media/dojo/1.3.0/lucidsite/index.js
@@ -1,25 +1,43 @@
 dojo.provide("lucidsite.index");
 dojo.require("dijit.layout.TabContainer");
 dojo.require("dijit.layout.ContentPane");
-dojo.require("dojox.widget.Dialog");
 dojo.require("dojox.av.FLVideo");
 dojo.require("dojox.av.widget.Player");
 dojo.require("dojox.av.widget.PlayButton");
 dojo.require("dojox.av.widget.VolumeButton");
 dojo.require("dojox.av.widget.ProgressSlider");
 dojo.require("dojox.av.widget.Status");
+dojo.require("dojox.fx.scroll");
+dojo.require("dojox.fx.easing");
+
+lucidsite.showVideo = function(){
+    var nl = dojo.query("#intro .video").addClass("shown");
+    setTimeout(dojo.hitch(dijit.byId("intro-video-player").children[0], "onClick"), 1500);
+    dojox.fx.smoothScroll({
+        node: "intro-video-player",
+        win: window,
+        easing: dojox.fx.easing.bounceOut,
+        duration: 800
+    }).play();
+}
+
+lucidsite.hideVideo = function(){
+    var v = dijit.byId("intro-video-player");
+    dojox.fx.smoothScroll({
+        node: document.body,
+        win: window,
+        easing: dojox.fx.easing.bounceOut,
+        duration: 800,
+        onEnd: function(){
+            v.media.pause();
+        }
+    }).play();
+    setTimeout(function(){
+        dojo.query("#intro .video").removeClass("shown");
+        if(v.media.isPlaying) v.children[0].onClick();
+    }, 200);
+}
 
 dojo.addOnLoad(function(){
-    dojo.query(".PlayerScreen", "intro-video-player").style({width: "896px", height: "504px"});
-    dojo.connect(dijit.byId("intro-video-dialog"), "show", function(){
-        setTimeout(dojo.hitch(dijit.byId("intro-video-player").children[0], "onClick"), 2000);
-    });
-    dojo.connect(dijit.byId("intro-video-dialog"), "hide", function(){
-        var v = dijit.byId("intro-video-player");
-        if(v.media.isPlaying) v.children[0].onClick();
-    });
-    setTimeout(function(){
-        dojo.place("intro-video-player", "intro-video-dialog", "first");
-    }, 200);
+    dojo.query(".PlayerScreen", "intro-video-player").style({width: "888px", height: "499px"});
 });
-
