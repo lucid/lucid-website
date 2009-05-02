40447778041f72a16451acae9d37051dec51c370
HFLW <me@hflw.org>
2009-05-02 16:32:07 -0400

    swapped dojo's player out with flowplayer for the time being

--- desktopsite/dojango/media/dojo/1.3.0/lucidsite/index.js
+++ desktopsite/dojango/media/dojo/1.3.0/lucidsite/index.js
@@ -1,23 +1,28 @@
 dojo.provide("lucidsite.index");
 dojo.require("dijit.layout.TabContainer");
 dojo.require("dijit.layout.ContentPane");
-dojo.require("dojox.av.FLVideo");
+/*dojo.require("dojox.av.FLVideo");
 dojo.require("dojox.av.widget.Player");
 dojo.require("dojox.av.widget.PlayButton");
 dojo.require("dojox.av.widget.VolumeButton");
 dojo.require("dojox.av.widget.ProgressSlider");
-dojo.require("dojox.av.widget.Status");
+dojo.require("dojox.av.widget.Status");*/
 dojo.require("dojox.fx.scroll");
 dojo.require("dojox.fx.easing");
 
 lucidsite.showVideo = function(){
     var nl = dojo.query("#intro .video").addClass("shown");
-    setTimeout(dojo.hitch(dijit.byId("intro-video-player").children[0], "onClick"), 1500);
+    //setTimeout(dojo.hitch(dijit.byId("intro-video-player").children[0], "onClick"), 1500);
     dojox.fx.smoothScroll({
         node: "intro-video",
         win: window,
         easing: dojox.fx.easing.bounceOut,
-        duration: 800
+        duration: 800,
+        onEnd: function(){
+            setTimeout(function(){
+                $f().play(); //flowplayer
+            }, 200);
+        }
     }).play();
 }
 
@@ -29,18 +34,40 @@ lucidsite.hideVideo = function(){
         easing: dojox.fx.easing.bounceOut,
         duration: 800,
         onEnd: function(){
-            v.media.pause();
+            //v.media.pause();
         }
     }).play();
     setTimeout(function(){
         dojo.query("#intro .video").removeClass("shown");
-        if(v.media.isPlaying) v.children[0].onClick();
+        //if(v.media.isPlaying) v.children[0].onClick();
     }, 200);
+    $f().pause(); //flowplayer
 }
 
 dojo.addOnLoad(function(){
-    dojo.query(".PlayerScreen", "intro-video-player").style({width: "888px", height: "499px"});
+    //dojo.query(".PlayerScreen", "intro-video-player").style({width: "888px", height: "499px"});
     //workaround
-    if(dojo.isWebKit)
-        setTimeout(function(){ dijit.byId("introTabs").resize(); }, 1000);
+    //if(dojo.isWebKit)
+    //    setTimeout(function(){ dijit.byId("introTabs").resize(); }, 1000);
+    
+    flowplayer("intro-video-player", "/site_media/flowplayer/flowplayer-3.1.0.swf");
+    $f("intro-video-player", "/site_media/flowplayer/flowplayer-3.1.0.swf", {
+        clip: { 
+            url: '/site_media/video/intro.flv', 
+            autoPlay: false, 
+            autoBuffering: true,
+            scaling: "orig"
+        }, 
+        plugins: { 
+            controls: {
+                url:  '/site_media/flowplayer/flowplayer.controls-3.1.0.swf',
+                fullscreen: false
+            }
+        }, 
+        onLoad: function(){
+            dojo.style(this.getParent(), "height", "522px");
+            $f().getScreen().css("height", "498px");
+            //dojo.byId($f().id()).childNodes[0].height="522px";
+        } 
+    });
 });
