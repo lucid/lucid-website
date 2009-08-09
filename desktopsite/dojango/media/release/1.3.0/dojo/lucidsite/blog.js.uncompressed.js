106c79956477758c869617eac35889c8027b6f0b
HFLW <me@hflw.org>
2009-08-09 14:40:28 -0400

    whoops

---
+++ desktopsite/dojango/media/release/1.3.0/dojo/lucidsite/blog.js.uncompressed.js
@@ -0,0 +1,27 @@
+/*
+	Copyright (c) 2004-2009, The Dojo Foundation All Rights Reserved.
+	Available via Academic Free License >= 2.1 OR the modified BSD license.
+	see: http://dojotoolkit.org/license for details
+*/
+
+/*
+	This is a compiled version of Dojo, built for deployment and not for
+	development. To get an editable version, please visit:
+
+		http://dojotoolkit.org
+
+	for documentation and information on getting the source.
+*/
+
+dojo.provide("lucidsite.blog");
+if(!dojo._hasResource["lucidsite.blog"]){ //_hasResource checks added by build. Do not use _hasResource directly in your code.
+dojo._hasResource["lucidsite.blog"] = true;
+dojo.provide("lucidsite.blog");
+
+//hide honeypot field
+dojo.addOnLoad(function() {
+	dojo.query("#comment_form_table tr:last-child").style("display", "none");
+});
+
+}
+
